8bc631c0b09ad995e29f94147371db842967f685
Grishma-M <maithaniagrishma@gmail.com>
2025-01-31 22:52:19 +0530

    cors 2 changes

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); 
 const express = require('express');
-const cors = require('cors'); // Import CORS
+const cors = require('cors'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Enable CORS (This should be before defining routes)
 app.use(cors());
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Import and use the routes
 const apiRoutes = require('./routes/apiRoutes');
 app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the GPRS Server!'); // A simple response for testing
+  res.send('Welcome to the GPRS Server!'); 
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
